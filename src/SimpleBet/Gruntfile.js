74d92dcfcac5bf991e339f41e544e1a13ca804e8
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-05-26 08:41:06 +0800

    Set up grunt file to copy to wwwroot

--- src/SimpleBet/Gruntfile.js
+++ src/SimpleBet/Gruntfile.js
@@ -1,24 +1,37 @@
-﻿module.exports = function (grunt) {
-    // load Grunt plugins from NPM
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+﻿'use-strict';
 
-    // configure plugins
-    grunt.initConfig({
-        uglify: {
-            my_target: {
-                files: { 'wwwroot/app.js': ['Scripts/app.js', 'Scripts/**/*.js'] }
-            }
-        },
+module.exports = function (grunt) {
+	// Load grunt tasks automatically
+	require('load-grunt-tasks')(grunt);
 
-        watch: {
-            scripts: {
-                files: ['Scripts/**/*.js'],
-                tasks: ['uglify']
-            }
-        }
-    });
+	// Time how long tasks take. Can help when optimizing build times
+	require('time-grunt')(grunt);
 
-    // define tasks
-    grunt.registerTask('default', ['uglify', 'watch']);
+	// configure plugins
+	grunt.initConfig({
+		pathConfig: {
+			app: 'Client',
+			webRoot: 'wwwroot',
+			bower: 'bower_components'
+		},
+
+		copy: {
+			code: {
+				expand: true, cwd: '<%= pathConfig.app %>/', src: ['**/*.js', '**/*.html', '**/*.css'], dest: '<%= pathConfig.webRoot%>/'
+			},
+			all: {
+				files: [
+					//all js, html, css, and assets
+					{expand: true, cwd: '<%= pathConfig.app %>/', src: ['**'], dest: '<%= pathConfig.webRoot%>/'},
+					//all dependencies
+					{expand: true, src: ['<%= pathConfig.bower %>/**'], dest: '<%= pathConfig.webRoot%>/'}
+				]
+			}
+		}
+	});
+
+	// define tasks
+	// grunt.registerTask('default', ['copy:all', 'copy:bower']);
+	grunt.registerTask('all', ['copy:all']);
+	grunt.registerTask('code', ['copy:code']);
 };
