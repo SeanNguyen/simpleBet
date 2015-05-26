23a6266c7233f19d6c431359f9e5140980cf554c
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-05-27 07:18:43 +0800

    added wagerTypeView

---
+++ src/SimpleBet/Client/assets/localhost_files/app.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+var app = angular.module('app', ['ngRoute']);
+
+app.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/', {
+				templateUrl: 'app/home/home.html',
+				controller: 'HomeController'
+			}).
+			when('/create', {
+				templateUrl: 'app/betCreator/betCreator.html',
+				controller: 'BetCreatorController'
+			}).
+			otherwise({
+				redirectTo: '/'
+			});
+	}]);
---
+++ src/SimpleBet/Client/assets/localhost_files/navbar.controller.js
@@ -0,0 +1,7 @@
+'use-strict';
+
+var app = angular.module('app');
+
+app.controller('NavbarController', function($scope) {
+	$scope.title = 'The Bet'
+});
