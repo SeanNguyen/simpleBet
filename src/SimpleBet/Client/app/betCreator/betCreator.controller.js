c13b3438e8d6bf1a70aaa580a0f6d17f6cc40740
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-05-27 17:46:13 +0800

    Fix some CSS

--- src/SimpleBet/Client/app/betCreator/betCreator.controller.js
+++ src/SimpleBet/Client/app/betCreator/betCreator.controller.js
@@ -3,7 +3,7 @@
 var app = angular.module('app');
 
 app.controller('BetCreatorController', function($scope) {
-    $scope.currentView = 0;
+    $scope.currentView = 2;
     $scope.question = 'What the colour of the next car passing by would be';
     $scope.options = [  {content: 'Maximum Number of characters is fifty incluing sp' },
                         {content: "It's a red car"}]
