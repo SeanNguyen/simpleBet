d9ab87d2b7bb079e2ba9ca6e629b5dfc186bc975
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-05-29 16:50:55 +0800

    Add validation for the timer

--- src/SimpleBet/Client/app/betCreator/rule/rule.controller.js
+++ src/SimpleBet/Client/app/betCreator/rule/rule.controller.js
@@ -3,5 +3,29 @@
 var app = angular.module('app');
 
 app.controller('ruleController', function($scope) {
+	$scope.Math = Math;
 	$scope.thirdNavbar = {title: 'Set Up', image: ''};
+	$scope.time = 2;//time in hours
+
+	$scope.increaseHour = function() {
+		$scope.time++;
+	};
+
+	$scope.decreaseHour = function() {
+		if ($scope.time > 0) {
+			$scope.time--;
+		}
+	};
+
+	$scope.increaseDay = function() {
+		$scope.time += 24
+	};
+
+	$scope.decreaseDay = function() {
+		if($scope.time > 24) {
+			$scope.time--;
+		} else {
+			$scope.time = 0;
+		}
+	};
 });
