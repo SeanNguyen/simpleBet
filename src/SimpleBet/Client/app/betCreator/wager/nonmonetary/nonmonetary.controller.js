ab3cb57bc23845c6397c1b0d8bbb306c39c6a7dc
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-05-29 11:21:11 +0800

    created secondary navbar and custom navbar

--- src/SimpleBet/Client/app/betCreator/wager/nonmonetary/nonmonetary.controller.js
+++ src/SimpleBet/Client/app/betCreator/wager/nonmonetary/nonmonetary.controller.js
@@ -1,5 +1,34 @@
 var app = angular.module('app');
 
 app.controller('nonmonetaryController', function($scope) {
-	$scope.tabs = ['tab1', 'tab2'];
+	$scope.tabs = [{title: 'Our Dares', imgage: ''}, {title: 'Custom Dares', image: ''}];	
+	$scope.currentTab = 0;
+	$scope.ourItems = [{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'A Noble King', description: 'Donate all of your valubles in your wallet to a nearby beggar.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'Static Shock ', description: 'Get eletricfied in a thunderstorm. Zap Zap pikachu I choose you.', avata: 'assets/icon_giftBox.png' },
+					{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' },
+					{title: 'Catch‘em All', description: 'Put on your business suit and seal the nearest 3 kids you see into a ball. (They must wear a tie.)', avata: 'assets/icon_giftBox.png' }]
+
+	$scope.customItems = [{title: 'Get Rekt Fool', description: 'Loser has to get wrecked like a wrecking ball. Wearing wrecking ball clothes.', avata: 'assets/x_button.png' },
+						{title: 'Self Input Title', description: 'Ut doluptatem demolessi dolescid qui alicit debitiore voloratibus ipsandandit la net quis receprat hiliquisquid que pererum quidipsam, con', avata: 'assets/x_button.png' },
+						{title: 'Self Input Title', description: 'Amenit ipsapitatem susdandebit ut il inus resto offictotas eum volume voluptatqui aut que core percius etur reperci psanienda qui venimet lam', avata: 'assets/x_button.png' }];
+
+	$scope.isTabselected = function (index) {
+		if(index === $scope.currentTab) {
+			return true;
+		}
+		return false;
+	}
+
+	$scope.setTab = function (index) {
+		$scope.currentTab = index;
+	}
 });
