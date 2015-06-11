97fbd16aac4e95e204398d754dfadf71ed6b3a43
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-06-11 14:10:15 +0800

    Change SQL Schema, add relation BetUser

---
+++ src/SimpleBet/Models/BetUser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBet.Models
+{
+    public enum BetUserState
+    {
+        NONE,
+        PENDING,
+        CONFIRMED,
+        CREATOR
+    };
+    
+    public class BetUser : Model
+    {
+        //Attributes
+        [Key, Column(Order = 0)]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int BetId { get; set; }
+        [ForeignKey("BetId")]
+        public Bet Bet { get; set; }
+
+        public BetUserState State { get; set; }
+
+        //Public Methods
+        public override Model parse(dynamic data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Model stringlify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
