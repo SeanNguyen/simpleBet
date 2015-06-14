48c64953650a1a44195ba527d174958fa94c64ad
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-06-14 14:59:19 +0800

    Added publish configs to arcadier Azure server

--- src/SimpleBet/Data/SimpleBetContext.cs
+++ src/SimpleBet/Data/SimpleBetContext.cs
@@ -2,6 +2,15 @@
 using System.Data.Entity;
 using System.Data.SqlClient;
 
+/* Connection string for azure server
+@"Server=tcp:pvuq1gyimf.database.windows.net,1433;Database=simplebet;
+                                        User ID=simplebet@pvuq1gyimf;Password=Simple123;
+                                        Trusted_Connection=False;
+                                        Encrypt=True;
+                                        Connection Timeout=30;
+                                        MultipleActiveResultSets=true;"
+*/
+
 namespace SimpleBet.Data
 {
     public class SimpleBetContext : DbContext
