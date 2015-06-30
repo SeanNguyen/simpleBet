31b2bcdc579bae3f7a8d95487f62f20596bcbda0
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-06-30 10:32:36 +0800

    added Wager Model

---
+++ src/SimpleBet/Models/WinningItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBet.Models
+{
+    public enum WINNING_ITEM_TYPE
+    {
+        MONETARY,
+        NONMONETARY
+    }
+
+    public class WinningItem : Model
+    {
+        public int Id { get; set; }
+        public WINNING_ITEM_TYPE Type { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int CreatorId { get; set; }
+        public User Creator { get; set; }
+        
+        //public methods
+        public override Model parse(dynamic data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Model stringlify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
