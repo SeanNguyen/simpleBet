c9aefb250aed3ed4d41b47c77229fb171dde67eb
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-06-30 18:37:31 +0800

    Added data seeder

---
+++ src/SimpleBet/Controllers/WinningItemController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+using SimpleBet.Data;
+using SimpleBet.Models;
+using SimpleBet.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SimpleBet.Controllers
+{
+    [Route("api/[controller]")]
+    public class WinningItemController : ApiController
+    {
+        //Attributes
+        private IDataService dataService = new DataService();
+
+        //Constructors
+        public WinningItemController() { }
+
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IList<WinningItem> winningItems = dataService.GetWinningItems();
+            string json = JsonConvert.SerializeObject(winningItems);
+            return Ok(json);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            WinningItem winningItem = dataService.GetWinningItem(id);
+
+            if (winningItem == null)
+            {
+                return new HttpNotFoundResult();
+            }
+            string json = JsonConvert.SerializeObject(winningItem);
+            return Ok(json);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]WinningItem winningItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            dataService.AddWinningItem(winningItem);
+            string json = JsonConvert.SerializeObject(winningItem);
+            return Ok(json);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]WinningItem winningItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != winningItem.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                this.dataService.UpdateWinningItem(winningItem);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (dataService.GetWinningItem(id) != null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            string json = JsonConvert.SerializeObject(winningItem);
+            return Ok(json);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                WinningItem winningItem = this.dataService.RemoveWinningItem(id);
+                string json = JsonConvert.SerializeObject(winningItem);
+                return Ok(json);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- src/SimpleBet/Data/Initializer.cs
+++ src/SimpleBet/Data/Initializer.cs
@@ -1,10 +1,62 @@
-﻿using System.Data.Entity;
+﻿using SimpleBet.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace SimpleBet.Data
 {
 
     public class Initializer : DropCreateDatabaseIfModelChanges<SimpleBetContext>
     {
+        protected override void Seed(SimpleBetContext context)
+        {
+            //Users
+            context.Users.Add(new User() { FacebookId = 123, Name = "Jon Snow", AvatarUrl = "someUrl" });
+            context.Users.Add(new User() { FacebookId = 321, Name = "I am a robot", AvatarUrl = "someUrl" });
+            context.SaveChanges();
+
+            //Winning Items
+            context.WinningItems.Add(new WinningItem()
+            {
+                Type = WINNING_ITEM_TYPE.NONMONETARY,
+                Title = "Punch",
+                Description = "I will punch you",
+                CreatorId = 1
+            });
+            context.WinningItems.Add(new WinningItem()
+            {
+                Type = WINNING_ITEM_TYPE.MONETARY,
+                Title = "Wine",
+                Description = "I will buy you this",
+                CreatorId = 2
+            });
+            context.SaveChanges();
+
+            //Bets
+            List<Option> options = new List<Option>();
+            options.Add(new Option() { Content = "This is option a" });
+            options.Add(new Option() { Content = "This is option b" });
+
+            List<BetUser> participations = new List<BetUser>();
+            participations.Add(new BetUser() { State = BetUserState.CONFIRMED, UserId = 1 });
+            participations.Add(new BetUser() { State = BetUserState.PENDING, UserId = 2 });
+
+            context.Bets.Add(new Bet()
+            {
+                BetType = BET_TYPE.ONE_MANY,
+                CreationTime = DateTime.Now,
+                Duration = 1000,
+                CreatorId = 1,
+                Question = "This is a question",
+                State = BET_STATE.CONFIRM,
+                Options = options,
+                Participations = participations,
+                //WinningItemId = 1
+            });
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
     }
 
 }
