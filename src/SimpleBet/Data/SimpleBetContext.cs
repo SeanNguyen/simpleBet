e8accdba291c704a272ceb982a3d722b351ca380
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-07-08 21:01:24 +0800

    added dynamic build for connectionString

--- src/SimpleBet/Data/SimpleBetContext.cs
+++ src/SimpleBet/Data/SimpleBetContext.cs
@@ -1,23 +1,29 @@
 ﻿using SimpleBet.Models;
 using System.Data.Entity;
 
-/* Connection string for azure server
-@"Server=tcp:pvuq1gyimf.database.windows.net,1433;Database=simplebet;
-                                        User ID=simplebet@pvuq1gyimf;Password=Simple123;
-                                        Trusted_Connection=False;
-                                        Encrypt=True;
-                                        Connection Timeout=30;
-                                        MultipleActiveResultSets=true;"
-*/
-
 namespace SimpleBet.Data
 {
     public class SimpleBetContext : DbContext
     {
-        public SimpleBetContext() : base(@"Data Source=(localdb)\mssqllocaldb;
+        //use different connection string for different build setting
+#if DEBUG
+        private static string connectionString = @"Data Source=(localdb)\mssqllocaldb;
                                         Initial Catalog=SimpleBet;
-                                        Integrated Security=True; Pooling=false;
-                                        MultipleActiveResultSets=true;")
+                                        Trusted_Connection=False;
+                                        Connection Timeout=30;
+                                        MultipleActiveResultSets=true;";
+#elif RELEASE
+        private static string connectionString = @"Server=tcp:pvuq1gyimf.database.windows.net,1433;
+                                        Database=simplebet; 
+                                        User ID=simplebet@pvuq1gyimf;
+                                        Password=Simple123;
+                                        Trusted_Connection=False;
+                                        Encrypt=True;
+                                        Connection Timeout=30;
+                                        MultipleActiveResultSets=true;";
+#endif
+
+        public SimpleBetContext() : base(connectionString)
         {
             Database.SetInitializer(new Initializer());
         }
