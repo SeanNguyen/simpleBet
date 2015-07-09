218958bbb4bbbb9bbb838eacca9a3dd25b518f9d
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-07-09 16:26:34 +0800

    update connectionString

--- src/SimpleBet/Data/SimpleBetContext.cs
+++ src/SimpleBet/Data/SimpleBetContext.cs
@@ -7,20 +7,17 @@ namespace SimpleBet.Data
     {
 
         //use different connection string for different build setting
+
         //private static string connectionString = @"Data Source=(localdb)\mssqllocaldb;
         //                                        Initial Catalog=SimpleBet;
         //                                        Trusted_Connection=False;
         //                                        Connection Timeout=30;
         //                                        MultipleActiveResultSets=true;";
 
-        private static string connectionString = @"Server=tcp:pvuq1gyimf.database.windows.net,1433;
-                                        Database=simplebet; 
-                                        User ID=simplebet@pvuq1gyimf;
-                                        Password=Simple123;
-                                        Trusted_Connection=False;
-                                        Encrypt=True;
-                                        Connection Timeout=30;
-                                        MultipleActiveResultSets=true;";
+        private static string connectionString = @"Server=tcp:pvuq1gyimf.database.windows.net,1433;Database=simplebet;
+                                                User ID = simplebet@pvuq1gyimf;Password=Simple123; Trusted_Connection=False;
+                                                Encrypt=True;Connection Timeout = 30;";
+        
 
         public SimpleBetContext() : base(connectionString)
         {
