94714b6683f179118c344fcba34c9a8472fb3850
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-07-14 18:22:03 +0800

    added profile page

---
+++ src/SimpleBet/Client/app/profile/profile.controller.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use-strict';
+    var app = angular.module('app');
+
+    var TABS = {
+        BASIC: 0,
+        CREDENTIAL: 1,
+        ADDRESS: 2
+    }
+
+    app.controller('profileController', function ($rootScope, $scope) {
+        $scope.currentTab = TABS.BASIC;
+
+        //functions
+        $scope.nextTab = nextTab;
+
+
+        //private
+        function nextTab() {
+            $scope.currentTab++;
+        }
+
+    });
+})();
--- src/SimpleBet/Client/app/profile/profile.js
+++ src/SimpleBet/Client/app/profile/profile.js
@@ -5,7 +5,8 @@ app.config(function($stateProvider) {
   // Now set up the states
   $stateProvider
     .state('root.profile', {
-      url: "/profile",
-      templateUrl: "app/profile/profile.html"
+        url: "/profile",
+        templateUrl: "app/profile/profile.html",
+        controller: 'profileController'
     })
 });
