ed0eb2e5abc0bb69a3d6c2ee937d403c05260b37
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-07-14 12:46:45 +0800

    update style for profile page

--- src/SimpleBet/Client/app/profile/profile.js
+++ src/SimpleBet/Client/app/profile/profile.js
@@ -4,7 +4,7 @@ var app = angular.module('app');
 app.config(function($stateProvider) {
   // Now set up the states
   $stateProvider
-    .state('profile', {
+    .state('root.profile', {
       url: "/profile",
       templateUrl: "app/profile/profile.html"
     })
