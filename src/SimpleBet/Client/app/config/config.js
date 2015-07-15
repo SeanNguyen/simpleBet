97805aeab90c8867e024c2e66fd1fb47d3172201
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-07-15 17:36:00 +0800

    added config for access from remote

--- src/SimpleBet/Client/app/config/config.js
+++ src/SimpleBet/Client/app/config/config.js
@@ -9,6 +9,8 @@
         var facebook = {};
         if ($location.host() === 'localhost' && $location.port() === 14248) {
             facebook.appId = '1480828435544588';
+        } if ($location.host() === '192.168.0.113' && $location.port() === 9000) {
+            facebook.appId = '1480828435544588';
         } else if ($location.host() === 'fluttr.azurewebsites.net') {
             facebook.appId = '1466436923650406';
         } else if ($location.host() === 'simplebet.arcadier.com') {
